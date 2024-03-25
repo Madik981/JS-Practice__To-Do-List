5da2fd6226c732fc14ba208e4867096ea10ab211
Madik981 <madi12312456@gmail.com>
2024-03-25 15:13:19 +0500

    improved the page layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const input = document.getElementById('input')
+const button_submit = document.getElementById('submit')
+const todo_task_count = document.getElementById('to-do__task-count')
+const inprogress_task_count = document.getElementById('in-progress__task-count')
+const closed_task_count = document.getElementById('closed__task-count')
+
+
+
+button_submit.onclick = () => {
+
+}
