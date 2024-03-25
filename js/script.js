49a046173e6d22eb12ef309bfb93c19b5b2c3239
Madik981 <madi12312456@gmail.com>
2024-03-25 20:06:19 +0500

    improved code readability in css and js, optimized some js functions, removed old test tags in html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// DOM Elements
 const inputElement = document.getElementById('input')
 const buttonSubmit = document.getElementById('submit')
-let buttonDelete = document.getElementById('list__button__delete')
 const toDoTask_countElement = document.getElementById('to-do__task-count')
 const inProgressTask_countElement = document.getElementById('in-progress__task-count')
 const closedTask_countElement = document.getElementById('closed__task-count')
@@ -8,10 +8,12 @@ const toDoList = document.getElementById('to-do__list')
 const inProgressList = document.getElementById('in-progress__list')
 const closedList = document.getElementById('closed__list')
 
+// Note arrays
 const toDoNotes = ['Сделать домашку']
 const inProgressNotes = ['Доделать проект']
 const closedNotes = ['Погулять с друзьями']
 
+// Functions
 buttonSubmit.onclick = () => {
     if(inputElement.value.length > 0){
         toDoNotes.push(inputElement.value)
